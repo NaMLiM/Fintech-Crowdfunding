06e037851108903d5e86383b8acc057d827538c3
Nafi'ul Anam <anamnafiul99@gmail.com>
2022-06-23 20:41:32 +0700

    update

--- index.php
+++ index.php
@@ -7,17 +7,12 @@ if (!isset($_GET['page'])) {
     $page = $_GET['page'];
 }
 ?>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>Crowdfunding</title>
-
     <!-- Scripts -->
     <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
