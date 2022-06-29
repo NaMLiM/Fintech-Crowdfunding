51c9981df48a9702450522e106f4e6a5f6f4b6cf
Nafi'ul Anam <anamnafiul99@gmail.com>
2022-06-29 18:12:49 +0700

    fix detail yayasan

--- yayasan.php
+++ yayasan.php
@@ -1,50 +1,58 @@
-<header>
-  <style>
-    .card {
-      margin: 10px;
-      width: 15rem;
-    }
-
-    .row {
-      justify-content: center;
-    }
-
-    .foto_yayasan {
-      width: 500px;
-    }
-  </style>
-</header>
-<div class="container" style="text-align: center; font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif">
-  <h2 style="font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif; ">WHAT IS CROWDFUNDING ?</h2>
-  <div class="row">
-    <div class="column">
-      <div class="card">
-        <img src="{{ asset('pic_yayasan/contoh.jpg') }}" class="card-img-top" alt="...">
-        <div class="card-body">
-          <p>penjelasan foto</p>
+<?php
+$id_yayasan = $_GET['id'];
+$select = mysqli_query($conn, "select * from yayasan where id_yayasan = '$id_yayasan'");
+while ($data = mysqli_fetch_array($select)) {
+    $nama_yayasan = $data["nama_yayasan"];
+    $alamat_yayasan = $data["alamat_yayasan"];
+    $pengasuh = $data["pengasuh_yayasan"];
+    $kondisi = $data["kondisi_yayasan"];
+    $no_wa = $data["no_whatsapp"];
+    $no_rek = $data["no_rekening"];
+    $nama_rek = $data["nama_rekening"];
+    $gambar = $data["gambar"];
+}
+?>
+<link rel="stylesheet" href="admin/css/template-2.css">
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-md-6 mb-5">
+            <div class="product-img border border-secondary">
+                <div id="gambar" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner" role="listbox">
+                        <?php
+                        $gambars = explode("-", $gambar);
+                        for ($i = 1; $i < count($gambars); $i++) {
+                        ?>
+                            <div class="carousel-item <?php if ($i == 1) {
+                                                            echo 'active';
+                                                        } ?>">
+                                <img src="<?php echo "admin/upload/" . $gambars[0] . "/gambar/" . $gambars[$i] ?>" class="d-block w-100">
+                            </div>
+                        <?php
+                        }
+                        ?>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#gambar" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#gambar" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </div>
         </div>
-      </div>
-
-
-      <div class="card">
-        <img src="{{ asset('pic_yayasan/contoh.jpg') }}" class="card-img-top" alt="...">
-        <div class="card-body">
-          <p>penjelasan foto</p>
+        <div class="col-md-6 text-dark">
+            <div class="mb-5">
+                <h3 class="mb-4"><?php echo $nama_yayasan ?></h3>
+                <?php echo $kondisi; ?>
+            </div>
+            <div class="text-dark mt-5">
+                <h3>Bantu Sekarang:</h3>
+                <a href=""><?php echo $no_rek . " a.n. " . $nama_rek ?></a>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
-  <div class="row">
-    <div class="column">
-      <h1>judul</h1>
-      <p>isi</p>
-
-      <div class="row">
-        <h2>nama</h2>
-        <h3>no. rek</h3>
-
-      </div>
-    </div>
-  </div>
-
 </div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.touchswipe/1.6.19/jquery.touchSwipe.min.js"></script>
